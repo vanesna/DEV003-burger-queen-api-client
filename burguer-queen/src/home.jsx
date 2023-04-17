32b53017e7940b03a7ba8df0ed2fbff7142a84fa
Francisca V <fcaavv@gmail.com>
2023-04-17 17:11:26 -0400

    feat: Add background and container with inputs to Home

---
+++ burguer-queen/src/home.jsx
@@ -0,0 +1,20 @@
+import logo from './assets/home.jpg';
+import './home.css';
+
+function Background() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${logo})`
+  };
+
+  return (
+    <div className="background-image" style={backgroundImageStyle}>
+      <h3 className="heading">Burguer Queen</h3>
+      <div className='container'>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="password" />
+      </div>
+    </div>
+  );
+}
+
+export default Background;
