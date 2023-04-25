9b2dac7fd44323b310401247d36fb97ab3ee3042
Vanessa <vanne.sna@gmail.com>
2023-04-25 15:59:28 -0600

    feat: add token and email at local storage

--- burguer-queen/src/components/login/login.jsx
+++ burguer-queen/src/components/login/login.jsx
@@ -2,20 +2,29 @@ import logo from '../../assets/home.jpg';
 import React, { useState, useEffect } from 'react';
 import './login.css';
 import { useForm } from 'react-hook-form';
-import { helpHttp } from '../../helpers/helpHttp'
+import axios from 'axios';
 
 export default function Home() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  let api = helpHttp();
+
   let urlUsers = 'http://localhost:8080/users'
 
   // useEffect(() => {
   //   api.get(urlUsers).then((res) =>
   //     console.log(res))
   // });
+
+  // axios({
+  //   method: 'get',
+  //   url: 'http://localhost:8080/users',
+  //   headers: {'X-Custom-Header': 'foobar'}
+  // })
+  //   .then(function (response) {
+  //     console.log('response axios', response.data.accessToken);
+  //   });
 
   const backgroundImageStyle = {
     backgroundImage: `url(${logo})`
@@ -24,11 +33,24 @@ export default function Home() {
 
   const onSubmit = (data) => {
     console.log(data);
+    axios.post('http://localhost:8080/login', data)
+      .then(function (response) {
+        console.log('token', response.data.accessToken);
+        console.log('user', response.data.user.email);
+        const token = response.data.accessToken;
+        const currentUser = response.data.user;
+        localStorage.setItem('accessToken', token)
+        localStorage.setItem('currentUser', JSON.stringify(response.data.user.email))
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
   }
 
   return (
     <div className="background-image" style={backgroundImageStyle}>
-      <h3 className="heading">Burguer Queen</h3>
+      <h3 className="heading">Burger Queen</h3>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className='container'>
           <input type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)} {...register('email', {
@@ -44,8 +66,8 @@ export default function Home() {
             minLength: 6,
             maxLength: 20,
           })} />
-          {errors.email?.type === 'pattern' && <p className="text-danger">Invalid Password</p>}
-          {errors.email?.type === 'required' && <p className="text-danger">Required</p>}
+          {errors.password?.type === 'pattern' && <p className="text-danger">Invalid Password</p>}
+          {errors.password?.type === 'required' && <p className="text-danger">Required</p>}
           <input type='submit' value='Login' className='login-button' />
         </div>
       </form>
