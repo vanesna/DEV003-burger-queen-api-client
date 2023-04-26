0fc0273035ad40335d6c8eeeb7f3e767828e747b
Vanessa <vanne.sna@gmail.com>
2023-04-25 22:26:34 -0600

    get all products with  axio

--- burguer-queen/src/components/menu/menu.jsx
+++ burguer-queen/src/components/menu/menu.jsx
@@ -1,12 +1,29 @@
 import '../login/login.css';
 import  Header  from '../Header/header';
+import axios from 'axios';
 
 export default function Breakfast() {
     
 
+  const token = localStorage.getItem('accessToken');
+  //console.log('token: ', token);
+
+  axios.get('http://localhost:8080/products', {
+    headers: {
+      'Authorization': `Bearer ${token}` 
+    }
+  })
+  .then((res) => {
+    console.log(res.data)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
     return ( 
     <div >
       <Header />
+      
       
     </div>
     )
