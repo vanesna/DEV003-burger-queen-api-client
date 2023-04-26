dbc042480a8bcac9e9a381edd9ad5acb7a3152e3
Francisca V <fcaavv@gmail.com>
2023-04-26 03:11:27 -0400

    refactor(routes): create route components instead of use createBrowserRouter. add PrivateRoute component with token logic. Redirect unauthenticated users to login. comment helpHttp

--- burguer-queen/src/helpers/helpHttp.js
+++ burguer-queen/src/helpers/helpHttp.js
@@ -1,56 +1,56 @@
-//peticion fetch
+// //peticion fetch
 
-export const helpHttp = () => {
+// export const helpHttp = () => {
 
-    const customFetch = (endpoint, options) => {
-        const defaultHeader = {
-            accept: "applicaction/json"
-        };
+//     const customFetch = (endpoint, options) => {
+//         const defaultHeader = {
+//             accept: "applicaction/json"
+//         };
 
-        const controller = new AbortController();
-        options.signal = controller.signal; //cancelar la peticion
+//         const controller = new AbortController();
+//         options.signal = controller.signal; //cancelar la peticion
 
-        options.method = options.method || 'GET';
-        options.headers = options.headers
-            ? { ...defaultHeader, ...options.headers }
-            : defaultHeader;
+//         options.method = options.method || 'GET';
+//         options.headers = options.headers
+//             ? { ...defaultHeader, ...options.headers }
+//             : defaultHeader;
 
-        options.body = JSON.stringify(options.body) || false;
-        if (!options.body) delete options.body;
+//         options.body = JSON.stringify(options.body) || false;
+//         if (!options.body) delete options.body;
 
-        console.log(options);
-        setTimeout(() => controller.abort(), 3000);
+//         console.log(options);
+//         setTimeout(() => controller.abort(), 3000);
 
-        return fetch(endpoint, options)
-            .then((res) => res.ok ? res.json() : Promise.reject({
-                err: true,
-                status: res.status || '00',
-                statusText: res.statusText || 'Ocurrio un error',
-            }))
-            .catch((error) => error);
-    };
+//         return fetch(endpoint, options)
+//             .then((res) => res.ok ? res.json() : Promise.reject({
+//                 err: true,
+//                 status: res.status || '00',
+//                 statusText: res.statusText || 'Ocurrio un error',
+//             }))
+//             .catch((error) => error);
+//     };
 
-//verbos http
-    const get = (url, options = {}) => {
-        customFetch(url, options)
-    };
+// //verbos http
+//     const get = (url, options = {}) => {
+//         customFetch(url, options)
+//     };
 
-    const post = (url, options = {}) => {
-        options.method = 'POST';
-        return customFetch(url, options);
-    };
+//     const post = (url, options = {}) => {
+//         options.method = 'POST';
+//         return customFetch(url, options);
+//     };
 
-    const put = (url, options = {}) => {
-        options.method = 'PUT';
-        return customFetch(url, options);
-    };
+//     const put = (url, options = {}) => {
+//         options.method = 'PUT';
+//         return customFetch(url, options);
+//     };
 
-    const del = (url, options = {}) => {
-        options.method = 'DELETE';
-        return customFetch(url, options);
-    };
+//     const del = (url, options = {}) => {
+//         options.method = 'DELETE';
+//         return customFetch(url, options);
+//     };
 
-    return {
-        get, post, put, del
-    };
-};
+//     return {
+//         get, post, put, del
+//     };
+// };
