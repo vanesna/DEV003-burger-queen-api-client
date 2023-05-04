c154961f7c5795fd65d574f81342a1237a21062d
Francisca V <fcaavv@gmail.com>
2023-05-04 11:00:21 -0400

    Refactored NavBar component and styles

---
+++ burguer-queen/src/components/navbar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './NavBar.css';
+
+export default function NavBar({
+  handleMenu,
+  setModalIsOpen,
+  handleCustomerName,
+}) {
+  return (
+    <nav className="navbar">
+      <div className="customer">
+        <input
+          type="text"
+          placeholder="Customer"
+          onChange={handleCustomerName}
+        />
+      </div>
+      <div className="button-group">
+        <button
+          value="Breakfast"
+          className="menu-btn"
+          onClick={(event) => handleMenu(event)}
+        >
+          Breakfast
+        </button>
+        <button
+          value="Lunch"
+          className="menu-btn"
+          onClick={(event) => handleMenu(event)}
+        >
+          Lunch
+        </button>
+        <button className="order" onClick={() => setModalIsOpen(true)}>
+          <i class="bi bi-clipboard2"></i>
+        </button>
+      </div>
+    </nav>
+  );
+}
