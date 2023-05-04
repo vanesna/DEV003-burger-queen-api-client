baffd8b3495a2299dcb932b1288d7b1c9dec95f9
Francisca V <fcaavv@gmail.com>
2023-05-04 11:04:28 -0400

    Refactor CardsProd component to receive products and handleAddToOrder as props

--- burguer-queen/src/components/cardsProd/cardsProd.jsx
+++ burguer-queen/src/components/cardsProd/cardsProd.jsx
@@ -1,57 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './cardsProd.css'
+// import React, { useState } from 'react';
+import './cardsProd.css';
 
-export default function CardsProd() {
-
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        getProducts(setProducts)
-    }, [])
-
-
-    async function getProducts() {
-
-        const token = localStorage.getItem('sessionToken');
-        //console.log('token: ', token);
-
-        await axios.get('http://localhost:8080/products', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-            .then((res) => {
-                const response = res.data;
-                setProducts(response);
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-    }
-
-    return (
-        <>
-            <div className='containerMenu'>
-                {products.map((product) => {
-                    return (
-
-                        <div className='card' key={product.id}>
-                            <img src={product.image} alt='' />
-                            <h3 >{product.name}</h3>
-                            <h3 className='cardPrice'>${product.price}</h3>
-                            <div className='quantity'>
-                                <button className='cardBtnMore'>+</button>
-                                <input className='cardInput' type='number' value={0} />
-                                <button className='cardBtnLess'>-</button>
-                            </div>
-
-                        </div>
-
-                    )
-                }
-                )}
-            </div >
-        </>
-    )
-};
+export default function CardsProd({ products, handleAddToOrder }) {
+  return (
+    <>
+      <div className="containerMenu">
+        {products.map((product) => {
+          return (
+            <div className="card" key={product.id}>
+              <img src={product.image} alt="" />
+              <h3>{product.name}</h3>
+              <h3 className="cardPrice">${product.price}</h3>
+              <div className="quantity">
+                <button
+                  className="cardBtnMore"
+                  onClick={() => handleAddToOrder(product.id)}
+                >
+                  +
+                </button>
+                {/* {counter} */}
+                <input className="cardInput" type="number" defaultValue={0} />
+                <button className="cardBtnLess">-</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
