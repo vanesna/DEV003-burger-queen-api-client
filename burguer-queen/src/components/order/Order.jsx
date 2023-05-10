8e1b7fe3bbe38503778fc8b4470adb6b71135efa
Francisca V <fcaavv@gmail.com>
2023-05-10 03:49:12 -0400

    refactor: update OrderForm component to display product details and calculate total price

--- burguer-queen/src/components/order/Order.jsx
+++ burguer-queen/src/components/order/Order.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import Product from './OrderList.jsx';
+import './Order.css';
 
 export default function OrderForm({ submitOrder, productsInOrder }) {
+  const totalPrice = productsInOrder.reduce(
+    (acc, product) => acc + product.price * product.quantity,
+    0,
+  );
+
   return (
-    <div>
+    <div className="order-content">
       <h1>Order</h1>
       <form onSubmit={submitOrder}>
         {productsInOrder.map((product) => (
-          <Product product={product} />
+          <div key={product.id} className="order-product">
+            <span className="order-quantity">{product.quantity}</span>
+            <span className="order-product-name">{product.name}</span>
+            <span className="order-product-price">
+              $ {product.price * product.quantity}
+            </span>
+          </div>
         ))}
-        {/* cantidad / producto / precio / borrar*/}
-        <button type="submit">Confirm</button>
+        <div className="order-total">
+          <span className='"order-product-total'>TOTAL:</span>{' '}
+          <span>${totalPrice}</span>
+        </div>
+        <div className="order-btn">
+          <button type="submit">Confirm</button>
+        </div>
       </form>
     </div>
   );
